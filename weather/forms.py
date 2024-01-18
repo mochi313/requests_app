4d814dcf327acd586237ef663bab24593826699e
mochi313 <147684137+mochi313@users.noreply.github.com>
2024-01-18 10:56:08 +0900

    first commit!!!!!!!!
    記念すべき最初のコミットです。

---
+++ weather/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.db import models
+
+# from weather.models import City
+
+# class CityChoices(models.TextChoices):
+#     OSAKA = 'osaka','大阪府'
+
+# class CityForm(forms.ModelForm):
+#     class Meta:
+#         model = City
+#         fields = ['city']
---
+++ weather/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render
+import requests
+import json
+# from .forms import CityForm
+# from .models import City
+
+# Create your views here.
+def index(request):
+    # city = City.objects.all()
+    # form = CityForm()
+    context = {
+        "message":"Hello World",
+        # "form":form,
+        # "city":city
+    }
+    return render(request,'weather/index.html',context)
+
+def get_weather(city):
+    cities = {
+        "北海道":"016010",
+        "青森県":"020010",
+        "岩手県":"030010",
+        "宮城県":"040010",
+        "秋田県":"050010",
+        "山形県":"060010",
+        "福島県":"070010",
+        "茨城県":"080010",
+        "栃木県":"090010",
+        "群馬県":"100010",
+        "埼玉県":"110010",
+        "千葉県":"120010",
+        "東京都":"130010",
+        "神奈川県":"1440010",
+        "新潟県":"150010",
+        "富山県":"160010",
+        "石川県":"170010",
+        "福井県":"180010",
+        "山梨県":"190010",
+        "長野県":"200010",
+        "岐阜県":"210010",
+        "静岡県":"220010",
+        "愛知県":"230010",
+        "三重県":"240010",
+        "滋賀県":"250010",
+        "京都府":"260010",
+        "大阪府":"270000",
+        "兵庫県":"280010",
+        "奈良県":"290010",
+        "和歌山県":"300010",
+        "鳥取県":"310010",
+        "島根県":"320010",
+        "岡山県":"330010",
+        "広島県":"340010",
+        "山口県":"350010",
+        "徳島県":"360010",
+        "香川県":"370010",
+        "愛媛県":"380010",
+        "高知県":"390010",
+        "福岡県":"400010",
+        "佐賀県":"410010",
+        "長崎県":"420010",
+        "熊本県":"430010",
+        "大分県":"440010",
+        "宮崎県":"450010",
+        "鹿児島県":"460010",
+        "沖縄県":"470010",
+    }
+    params = cities[city]
+    url = f"https://weather.tsukumijima.net/api/forecast?city={params}"
+    response = requests.get(url,verify=False)
+    data = response.json()
+    return data
+
+def wether(request):
+    city = request.GET("address")
+    data = get_weather("大阪府")["forecasts"][0]
+    context = {
+        "weathers":data,
+        "date":data["date"],
+        "telop":data["telop"],
+        "image":{
+            "tag":data["image"]["title"],
+            "url":data["image"]["url"],
+            "city":city
+        },
+    }
+    return render(request,'weather/weather.html',context)
